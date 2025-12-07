132e6b0c23ddd3604fb242615e2f7243ba6e859c
Bissmella Bahaduri <bissmellabahaduri@gmail.com>
2025-12-07 16:53:04 +0100

    debugging

--- src/diffusers/models/attention_dispatch.py
+++ src/diffusers/models/attention_dispatch.py
@@ -1323,8 +1323,8 @@ def TemplatedUnifiedAttention(
     ring_mesh = _parallel_config.context_parallel_config._ring_mesh
     ring_group = ring_mesh.get_group()
     #hardcoded for now
-    scatter_idx = 2
-    gather_idx = 1
+    scatter_idx = 1
+    gather_idx = 2
 
     query = SeqAllToAllDim.apply(ulysses_group, query, scatter_idx, gather_idx)
     key = SeqAllToAllDim.apply(ulysses_group, key, scatter_idx, gather_idx)
